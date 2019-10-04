573cb1cfef33489fa2edd2db10d9b8cf6a457cc9
LunaOlbrechts <55496372+LunaOlbrechts@users.noreply.github.com>
2019-10-04 14:47:15 +0200

    Add comment to code

--- Shelter.Shared/Program.cs
+++ Shelter.Shared/Program.cs
@@ -10,13 +10,14 @@ namespace Shelter.Shared
     {
         static void Main(string[] args)
         {   
+            //make new object of shelter
             var DierenbeschermingMechelen = new Shelter() {Name="Dierenbescherming Mechelen"};
-
+            // make new object of person
             var Owner = new Person {FirstName="Thomas", LastName="Van Hemelrijk"};
             Console.WriteLine($"{Owner.FirstName} {Owner.LastName}");
 
             DierenbeschermingMechelen.Animals.Add(new Dog() {Name="Koda", DateOfBirth="11-02-2017", IsChecked=true, KidFriendly=true, Since="02-10-2019", Race="Husky", Barker=false});
-           
+           // print list of animals
             DierenbeschermingMechelen.Animals.ToList().ForEach(x => 
             {
                 Console.WriteLine($"{x.Name} {x.DateOfBirth} - Checked: {x.IsChecked} - Kid friendly: {x.KidFriendly} - In shelter since: {x.Since} - Race: {x.Race} - Barker: ");
--- Shelter.Shared/Shelter.cs
+++ Shelter.Shared/Shelter.cs
@@ -8,7 +8,9 @@ namespace Shelter.Shared
     class Shelter{
         public string Name {get; set;}
         public Person Owner {get; set;}
+        public string Adress {get; set;}
         public ICollection<Animal> Animals {get; set;}
         public ICollection<Person> Employees {get; set;}
+
     }
 }
