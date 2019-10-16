a444e6d8d79254a87b044294af347452c28dc72c
LunaOlbrechts <55496372+LunaOlbrechts@users.noreply.github.com>
2019-10-16 15:51:47 +0200

    Mvc

--- Mvc/Controllers/ShelterController.cs
+++ Mvc/Controllers/ShelterController.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Mvc.Models;
+using Shelter.Mvc.Models;
+using Shelter.Shared;
 
-namespace Mvc.Controllers
+namespace Shelter.Mvc.Controllers
 {
     public class ShelterController : Controller
     {
@@ -18,20 +14,9 @@ namespace Mvc.Controllers
             _logger = logger;
         }
 
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
