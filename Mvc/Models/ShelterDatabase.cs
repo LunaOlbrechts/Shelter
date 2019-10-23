ab11e0b6017081b75d1fbf98b8795490629c1bb3
HannahDeWachter <55497252+HannahDeWachter@users.noreply.github.com>
2019-10-23 15:38:22 +0200

    edit animals

--- Mvc/Models/ShelterDatabase.cs
+++ Mvc/Models/ShelterDatabase.cs
@@ -15,9 +15,9 @@ namespace Mvc.Models
                 var DierenbeschermingMechelen = new Shelter.Shared.Shelter()
                 {
                     Animals = new List<Animal> {
-                      new Dog() { Name = "Koda", DateOfBirth = "11-02-2017", IsChecked = true, KidFriendly = true, Since = "02-10-2019", Race = "Husky", Barker = false, Id=1},
-                      new Dog() { Name = "Beatle", DateOfBirth = "11-02-2017", IsChecked = true, KidFriendly = true, Since = "02-10-2019", Race = "Husky", Barker = false, Id=2},
-                      new Dog() { Name = "Bahia", DateOfBirth = "11-02-2017", IsChecked = true, KidFriendly = true, Since = "02-10-2019", Race = "Husky", Barker = false, Id=3},
+                      new Dog() { Name = "Koda", DateOfBirth = "11-02-2017", IsChecked = true, KidFriendly = true, Since = "02-09-2019", Race = "Husky", Barker = false, Id=1},
+                      new Dog() { Name = "Beatle", DateOfBirth = "06-10-2012", IsChecked = false, KidFriendly = true, Since = "15-10-2018", Race = "Beatle", Barker = true, Id=2},
+                      new Dog() { Name = "Bahia", DateOfBirth = "14-09-2015", IsChecked = true, KidFriendly = true, Since = "07-03-2019", Race = "Dalmatian", Barker = false, Id=3},
                     }
                 };
 
