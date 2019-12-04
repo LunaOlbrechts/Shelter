f75094bc59618de61fd5610c07ef628c3a2db70e
LunaOlbrechts <55496372+LunaOlbrechts@users.noreply.github.com>
2019-12-04 16:58:45 +0100

    fix duuussd errors

--- Shelter.UnitTests/UnitTestBasic.cs
+++ Shelter.UnitTests/UnitTestBasic.cs
@@ -1,7 +1,7 @@
 using NUnit.Framework;
-using Shelter.MVC.Controllers;
+using Mvc.Controllers;
 using Moq;
-using Shelter.MVC;
+using Mvc;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
