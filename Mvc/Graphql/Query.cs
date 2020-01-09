cdf22416c93a2e98b220da6df753d671d8a995dc
LunaOlbrechts <55496372+LunaOlbrechts@users.noreply.github.com>
2020-01-09 19:32:54 +0100

    Graphql query class

--- Mvc/Graphql/Query.cs
+++ Mvc/Graphql/Query.cs
@@ -8,27 +8,56 @@ using Microsoft.EntityFrameworkCore;
 using Shelter.Shared;
 
 
-namespace Mvc.Graphql 
+namespace Mvc.Graphql
 {
-  public class Query
-  {
-
-    [GraphQLMetadata("animals")]
-    public IEnumerable<Animal> GetAnimals()
+    public class Query
     {
-      return Enumerable.Empty<Books>();
-    }
 
-    [GraphQLMetadata("shelters")]
-    public IEnumerable<Shelters> GetShelters() 
-    {
-      return Enumerable.Empty<Authors>();
-    }
+        [GraphQLMetadata("shelters")]
+        public IActionResult GetAllShelters()
+        {
+            return Ok(_dataAccess.GetAllShelters());
+        }
 
-    [GraphQLMetadata("author")]
-    public Author GetAuthor(int id)
-    {
-      return null;
+        [GraphQLMetadata("sheltersFull")]
+        public IActionResult GetAllSheltersFull()
+        {
+            // You return a list here, "not found" is not an issue -- an empty list is still a valid list.
+            return Ok(_dataAccess.GetAllSheltersFull());
+        }
+
+        [GraphQLMetadata("idShelter")]
+        public IActionResult GetShelter(int id)
+        {
+            // find shelter in database if the shelter does not excist trow 404 Not found
+            var shelter = _dataAccess.GetShelterById(id);
+
+            if (shelter == null)
+            {
+                return NotFound("404 shelter is not found");
+            }
+            return Ok(shelter);
+        }
+
+        [GraphQLMetadata("animals")]
+        public IActionResult GetShelterAnimals(int id)
+        {
+            // if you don't find the shelter, return a 404. Again, an empty list is an empty list so empty list of animal is a valid result.
+            var animals = _dataAccess.GetAnimals(id);
+            return animals == default(IEnumerable<Animal>) ? (IActionResult)NotFound("404 the Shelter is not found") : Ok(animals);
+        }
+
+         [GraphQLMetadata("animal")]
+         public IActionResult GetAnimalDetails(int shelterId, int animalId)
+        {
+            var animal = _dataAccess.GetAnimalByShelterAndId(shelterId, animalId);
+            var shelter = _dataAccess.GetShelterById(shelterId);
+
+            if (animal == null || shelter == null)
+            {
+                return NotFound("404 shelter or/and animal are not found");
+            }
+            return Ok(animal);
+        }
     }
-  }
 }
